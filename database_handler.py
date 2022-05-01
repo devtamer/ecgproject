fdf55e068859b2ca6b040153f34b0681fd95ef2e
Matthew Sheehan <matthewsheehan@Matthews-Mac-mini.local>
2022-05-01 16:48:55 -0700

    Add login and register page

---
+++ database_handler.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+class Database_Handler():
+
+    # TODO: Before working on this class, set up a database table for a User (ID, FName, LName, Email, Pass)
+
+    # TODO: Connect to database file
+    def __init__(self):
+        pass
+
+    # TODO: Set up a SQL query (and action) to insert user into DB using a Python dictionary.
+    def insert_user(self, user):
+        pass
