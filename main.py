a0a232ad8a3c2061e2ffc8ffd7819583c63eb03c
Matthew Sheehan <matthewsheehan@Matthews-Mac-mini.local>
2022-05-03 17:11:34 -0700

    Add ECG image to account page

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_bcrypt import Bcrypt
 import sqlite3 as sql
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @app.route("/")
 @app.route("/home")
@@ -18,14 +20,14 @@ def register():
                 "FirstName" : request.form['fname'],
                 "LastName" : request.form['lname'],
                 "Email" : request.form['email'],
-                "Password" : request.form['pass']
+                "Password" : bcrypt.generate_password_hash(request.form['pass'])
             }   
             with sql.connect("HeartHealth.db") as con:
                 cur = con.cursor()
                 cur.execute("INSERT INTO user (FirstName, LastName, Email, Password) VALUES(?,?,?,?)", (user["FirstName"], user["LastName"], user["Email"], user["Password"]))
                 con.commit()
                 con.close()
-        except sql.Error as error:
+        except sql.Error:
             return render_template('404.html', error="SQL Error")
         finally:
             return redirect(url_for('login'))
@@ -43,7 +45,7 @@ def login():
                 for row in results:
                     email = row[3]
                     password = row[4]
-                    if ((request.form['email'] == email) and (request.form['pass'] == password)):
+                    if ((request.form['email'] == email) and (bcrypt.check_password_hash(password, request.form['pass']))):
                         return redirect(url_for('account', id=row[0]))
         except sql.Error:
             return render_template('404.html', error="SQL Error")
@@ -60,8 +62,8 @@ def account(id):
                 "LastName" : result[2],
                 "Email" : result[3]
             }  
-            return render_template('account.html', user=user)
-    except sql.Error as error:
+            return render_template('account.html', user=user, id=id)
+    except sql.Error:
         return render_template('404.html', error="SQL Error")
         
 if __name__ == '__main__':
